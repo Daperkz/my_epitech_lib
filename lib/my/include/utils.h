310026ba124b3ab015ad0fcfdbca48e1041edeac
August Conchin <august.conchin@epitech.eu>
2025-12-12 15:07:23 +0100

    feat: defined the bases (2, 7, 10, 15) as macros for code readability

--- lib/my/include/utils.h
+++ lib/my/include/utils.h
@@ -10,6 +10,11 @@
 
     #define UNUSED(arg) ((void)arg)
 
+    #define base2 "01"
+    #define base7 "01234567"
+    #define base10 "0123456789"
+    #define base15 "0123456789ABCDEF"
+
 void my_swap(int *a, int *b);
 void my_sort_int_array(int *tab, int size);
 char *concat_params(int argc, char **argv);
