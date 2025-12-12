8b27fff10578cbc01de9fa6e84beeb9d804cf982
August Conchin <august.conchin@epitech.eu>
2025-12-12 15:09:17 +0100

    feat: defined the bases (2, 7, 10, 15) as macros for code readability

--- lib/my/include/utils.h
+++ lib/my/include/utils.h
@@ -13,7 +13,8 @@
     #define BASE2 "01"
     #define BASE7 "01234567"
     #define BASE10 "0123456789"
-    #define BASE15 "0123456789ABCDEF"
+    #define BASE15CAP "0123456789ABCDEF"
+    #define BASE15LOW "0123456789abcdef"
 
 void my_swap(int *a, int *b);
 void my_sort_int_array(int *tab, int size);
