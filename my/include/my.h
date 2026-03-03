a803d5c6c99b611756a9696dfed6c7ec9b8cd625
August Conchin <august.conchin@epitech.eu>
2026-03-03 20:22:03 +0100

    feat(header): add macro header for basic exit outputs

--- my/include/my.h
+++ my/include/my.h
@@ -8,11 +8,15 @@
 #ifndef MY_H_
     #define MY_H_
 
+    #include "my/macro.h"
+
     #include "my/file.h"
     #include "my/io.h"
     #include "my/math.h"
     #include "my/printf.h"
     #include "my/string.h"
     #include "my/utils.h"
+
+    #include "my/linked_lists.h"
 
 #endif /* !MY_H_ */
---
+++ my/include/my/macro.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2026
+** macro.h
+** File description:
+** macro
+*/
+
+#ifndef MACRO_H_
+    #define MACRO_H_
+
+    #define EXIT_SUCCESS 0
+    #define EXIT_FAILURE 1
+    #define EXIT_ERROR 84
+
+#endif /* !MACRO_H_ */
