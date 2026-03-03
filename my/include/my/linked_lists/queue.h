c301ccd7b41791d9318220d30664d06bdb1e4ef1
August Conchin <august.conchin@epitech.eu>
2026-03-03 22:10:56 +0100

    feat(link_list): defines were not indented

--- my/include/my/linked_lists/queue.h
+++ my/include/my/linked_lists/queue.h
@@ -12,10 +12,10 @@
 
 typedef dll_t queue_t;
 
-#define QUEUE_CREATE() dll_create()
-#define QUEUE_ENQUEUE(q, data) dll_insert(q, data)
-#define QUEUE_DEQUEUE(q) dll_pop_tail(q)
-#define QUEUE_IS_EMPTY(q) ((q)->len == 0)
+    #define QUEUE_CREATE() dll_create()
+    #define QUEUE_ENQUEUE(q, data) dll_insert(q, data)
+    #define QUEUE_DEQUEUE(q) dll_pop_tail(q)
+    #define QUEUE_IS_EMPTY(q) ((q)->len == 0)
 
 
 #endif /* !QUEUE_H_ */
--- my/include/my/linked_lists/stack.h
+++ my/include/my/linked_lists/stack.h
@@ -12,9 +12,9 @@
 
 typedef ll_t stack_t;
 
-#define STACK_CREATE() ll_create()
-#define STACK_PUSH(s, data) ll_insert(s, data)
-#define STACK_POP(s) ll_pop_head(s)
-#define STACK_IS_EMPTY(s) ((s)->len == 0)
+    #define STACK_CREATE() ll_create()
+    #define STACK_PUSH(s, data) ll_insert(s, data)
+    #define STACK_POP(s) ll_pop_head(s)
+    #define STACK_IS_EMPTY(s) ((s)->len == 0)
 
 #endif /* !STACK_H_ */
