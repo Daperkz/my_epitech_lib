368facaadc65b91b01b4962e26a5f2ffaf1bf6f1
August Conchin <august.conchin@epitech.eu>
2026-03-03 20:47:28 +0100

    feat(link_list): add get function for link_list

---
+++ my/src/linked_lists/simple/get.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2026
+** get.c
+** File description:
+** get
+*/
+
+#include "my/macro.h"
+
+#include "my/linked_lists/simple_linked_list.h"
+
+void *ll_get(ll_t *list, void *ref, int (*cmp)(void *, void *))
+{
+    node_t *tmp = NULL;
+
+    if (!list || !cmp)
+        return (NULL);
+    tmp = list->head;
+    for (int i = 0; i < list->len; i++) {
+        if (cmp(tmp->data, ref) == 0)
+            return (tmp->data);
+        tmp = tmp->next;
+    }
+    return (NULL);
+}
