9f6ca61ceb52ed548723342d528592107a2c878f
August Conchin <august.conchin@epitech.eu>
2026-03-03 20:48:20 +0100

    feat(header): add function pop_head prototype

--- my/include/my/linked_lists/simple_linked_list.h
+++ my/include/my/linked_lists/simple_linked_list.h
@@ -21,9 +21,9 @@ void ll_destroy(ll_t *list, void (*free_data)(void *));
 int ll_append(ll_t *list, void *data);
 int ll_insert(ll_t *list, void *data);
 
-void *ll_pop_head(ll_t *list);
-
 void *ll_get(ll_t *list, void *ref, int (*cmp)(void *, void *));
+
+void *ll_pop_head(ll_t *list);
 void *ll_pop(ll_t *list, void *ref, int (*cmp)(void *, void *));
 
 #endif /* !SIMPLE_LINKED_LIST_H_ */
