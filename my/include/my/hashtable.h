1840abf0393d5eacb9dd3c993e20f28b6a2ffe12
August Conchin <august.conchin@epitech.eu>
2026-03-06 21:27:00 +0100

    feat(hashtable):
    	- add bucket create and destroy function
    	- add more hash functions
    	- change certain prototypes of function

--- my/include/my/hashtable.h
+++ my/include/my/hashtable.h
@@ -8,12 +8,15 @@
 #ifndef HASHTABLE_H
     #define HASHTABLE_H
 
-    #include "simple_linked_list.h"
+    #include "linked_lists/simple_linked_list.h"
 
 typedef struct ht_buckets_s {
     char *key;
-    void *value;
+    void *data;
 } ht_buckets_t;
+
+ht_buckets_t *bucket_create(char *key, void *data);
+void *bucket_destroy(ht_buckets_t *bucket);
 
 typedef struct hashtable_s {
     int (*hash)(char *, int);
@@ -21,20 +24,23 @@ typedef struct hashtable_s {
     ll_t **buckets;
 } ht_t;
 
-// Hash function
-int sdbm(char *key);
-int fnv_1a(char *key);
-int djb2(char *key);
+// Hash functions
+
+int sdbm(char *key, int len);
+int fnv_1a(char *key, int len);
+int djb2(char *key, int len);
 int hash(char *key, int len);
 
 // Create & destro table
-ht_t *ht_create(int (*hash)(char *, int), int len);
-void ht_destroy(ht_t *ht);
+
+ht_t *ht_create(int (*hash)(char *, int), int size);
+void ht_destroy(ht_t *ht, void (*free_data)(void *));
 
 // Handle table
+
 int ht_insert(ht_t *ht, char *key, void *data);
-int ht_delete(ht_t *ht, char *key);
-char *ht_search(ht_t *ht, char *key);
+void *ht_pop(ht_t *ht, char *key);
+void *ht_get(ht_t *ht, char *key);
 void ht_dump(ht_t *ht, void (*dump_data)(void *));
 
 #endif /* HASHTABLE_H */
