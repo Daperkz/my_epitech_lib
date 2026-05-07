75738dc0c212d0d8e83f84ae7958477f1b689f8d
August Conchin <august.conchin@epitech.eu>
2026-05-07 18:16:13 +0200

    feat(header): doxygened file.h
    	- add doxygens comment to all functions of file.h

--- dkz/include/dkz/file.h
+++ dkz/include/dkz/file.h
@@ -8,14 +8,61 @@
 #ifndef DKZ_FILE_H_
     #define DKZ_FILE_H_
 
+/**
+** @brief Opens a file and returns its file descriptor
+**
+** @param[in] filename The path to the file to open
+**
+** @return @b int The file descriptor, or -1 if the file cannot be opened
+*/
 int open_file(char const *filename);
 
+/**
+** @brief Gets the size of a file using stat
+**
+** @param[in] filename The path to the file
+**
+** @return @b long The size of the file in bytes, or -1 on error
+*/
 long get_file_size(char const *filename);
+
+/**
+** @brief Gets the size of a file by reading it (useful for special files)
+**
+** @param[in] filename The path to the file
+**
+** @return @b long The size of the file in bytes, or -1 on error
+*/
 long get_file_size_dyn(char const *filename);
 
+/**
+** @brief Reads a specific number of bytes from a file descriptor
+**
+** @param[in] fd The file descriptor to read from
+** @param[in] size The number of bytes to read
+**
+** @return @b char* A new allocated string containing the content, or NULL on error
+*/
 char *get_file_content(int fd, long size);
 
+/**
+** @brief Reads the entire content of a file into a string using stat
+**
+** @param[in] filename The path to the file
+**
+** @return @b char* A new allocated string with file content, or NULL on error
+*/
 char *read_file(char const *filename);
+
+/**
+** @brief Reads the entire content of a file dynamically (without stat)
+**
+** @param[in] filename The path to the file
+**
+** @return @b char* A new allocated string with file content, or NULL on error
+**
+** @note Useful for files where stat fails to report size, like /dev or /proc
+*/
 char *read_file_dyn(char const *filename);
 
 #endif /* !DKZ_FILE_H_ */
