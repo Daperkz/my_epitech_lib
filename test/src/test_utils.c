0a9198e9616a1ab0ab40208d12d966a161b75417
August Conchin <august.conchin@epitech.eu>
2026-06-14 19:18:34 +0200

    feat(test): utils 100% coverage
    	- add 1 test for concat_params (malloc check).
    	- chore : make the convert_base function.

--- test/src/test_utils.c
+++ test/src/test_utils.c
@@ -5,16 +5,13 @@
 ** test_utils
 */
 
-#include <criterion/criterion.h>
-#include <criterion/redirect.h>
-#include <stdlib.h>
-
-#include "dkz/utils.h"
+#include "test.h"
 
 Test(my_swap, swap_integers)
 {
     int a = 42;
     int b = 21;
+
     my_swap(&a, &b);
     cr_assert_eq(a, 21);
     cr_assert_eq(b, 42);
@@ -23,6 +20,7 @@ Test(my_swap, swap_integers)
 Test(my_sort_int_array, sort_basic)
 {
     int tab[] = {5, 2, 8, 1, 9};
+
     my_sort_int_array(tab, 5);
     cr_assert_eq(tab[0], 1);
     cr_assert_eq(tab[4], 9);
@@ -31,6 +29,7 @@ Test(my_sort_int_array, sort_basic)
 Test(my_sort_int_array, sort_empty)
 {
     int *tab = NULL;
+
     my_sort_int_array(tab, 0);
     cr_assert_eq(tab, NULL);
 }
@@ -39,6 +38,16 @@ Test(concat_params, basic_concat)
 {
     char *argv[] = {"./test", "hello", "world"};
     char *res = concat_params(3, argv);
+
     cr_assert_str_eq(res, "./test\nhello\nworld");
     free(res);
+}
+
+Test(concat_params, malloc_fail, .fini = reset_malloc_wrapper)
+{
+    char *argv[] = {"./test", "hello", "world"};
+    
+    set_malloc_fail(1);
+    set_malloc_count(0);
+    cr_assert_null(concat_params(3, argv));
 }
